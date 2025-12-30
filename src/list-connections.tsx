6532a3038c6e5343165281bb81a0a440c2aa63db
Yigit Efe <yiitefe@gmail.com>
2025-12-30 02:45:50 +0100

    refactor: move `useConnections` to dedicated hooks folder

--- src/list-connections.tsx
+++ src/list-connections.tsx
@@ -4,7 +4,7 @@ import { Connection, ConnectionState } from "@/types"
 import { connect, disconnect, waitForConnectionState } from "@/api/viscosity"
 import { ActionTitle, Error, Message, SectionTitle } from "@/constants"
 import { setQuickConnect } from "@/api/storage"
-import { useConnections } from "@/use-connections"
+import { useConnections } from "@/hooks/use-connections"
 import { ConnectionListItem } from "@/components/connection-list-item"
 
 export default function Command() {
